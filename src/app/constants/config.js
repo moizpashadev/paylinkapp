b1206029df1400c3775220a30bb072173a01402b
moizpashadev <moizpasha.developer@gmail.com>
2025-01-24 16:54:25 +0500

    bugfixess

--- src/app/constants/config.js
+++ src/app/constants/config.js
@@ -7,9 +7,14 @@ const config = {
     gatewayUrl: 'https://testcheckout.kuickpay.com',
   },
   production: {
-    appUrl: 'https://paymentlink.kuickpay.com',
-    gatewayUrl: 'https://checkout.kuickpay.com',
+    // appUrl: 'https://paymentlink.kuickpay.com',
+    // gatewayUrl: 'https://checkout.kuickpay.com',
+    appUrl: 'https://uat-paymentlink.kuickpay.com',
+    gatewayUrl: 'https://testcheckout.kuickpay.com',
   },
+
+
+  
 };
 
 export const API_URLS = config[environment];
