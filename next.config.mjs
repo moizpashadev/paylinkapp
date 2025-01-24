ade38e81c0a731726b7073845b7c0e132dca7183
moizpashadev <moizpasha.developer@gmail.com>
2025-01-24 17:24:37 +0500

    bugfixess

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         domains: ['app2.kuickpay.com'], // Add your image host domain here
-//     },
-// };
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'app2.kuickpay.com',
-            },
-        ],
+        domains: ['app2.kuickpay.com'], // Add your image host domain here
     },
 };
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'app2.kuickpay.com',
+//             },
+//         ],
+//     },
+// };
 
 export default nextConfig;
